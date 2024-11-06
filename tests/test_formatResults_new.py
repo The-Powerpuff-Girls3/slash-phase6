d402987a5ee73b9ccbd01e5807aab27a176e8012
TheFatBlue <thefatblue@gmail.com>
2024-11-05 23:07:35 -0500

    clean up code and add comments for tests

--- tests/test_formatResults_new.py
+++ tests/test_formatResults_new.py
@@ -28,15 +28,6 @@ def test_sortList_same_prices():
     assert formatter.sortList(arr, "pr", False) == ansArr
 
 
-# def test_sortList_missing_price_field():
-#     """
-#     Checks sortList where one dictionary is missing a price field
-#     """
-#     arr = [{"price": "$10"}, {"price": "$5"}, {}]
-#     ansArr = [{}, {"price": "$5"}, {"price": "$10"}]
-#     assert formatter.sortList(arr, "pr", False) == ansArr
-
-
 def test_sortList_mixed_currency_format():
     """
     Checks sortList with mixed currency format in prices
@@ -61,18 +52,6 @@ def test_formatResults_multiple_titles():
     assert product["title"] == "title 1"
 
 
-# def test_formatResults_no_price():
-#     """
-#     Checks formatResults with no price entry
-#     """
-#     titles = [BeautifulSoup('<div class="title">Sample Title</div>', "html.parser")]
-#     prices = []
-#     links = []
-#     images = []
-#     product = formatter.formatResult("example", titles, prices, links, images)
-#     assert product["price"] == "N/A"
-
-
 def test_formatResults_special_characters_title():
     """
     Checks formatResults with special characters in title
@@ -83,30 +62,6 @@ def test_formatResults_special_characters_title():
     images = []
     product = formatter.formatResult("example", titles, prices, links, images)
     assert product["title"] == "Special & Char Title"
-
-
-# def test_formatResults_empty_title():
-#     """
-#     Checks formatResults with an empty title entry
-#     """
-#     titles = [BeautifulSoup('<div class="title"></div>', "html.parser")]
-#     prices = [BeautifulSoup('<div class="price">$0.99</div>', "html.parser")]
-#     links = []
-#     images = []
-#     product = formatter.formatResult("example", titles, prices, links, images)
-#     assert product["title"] == "N/A"
-
-
-# def test_formatResults_incomplete_data():
-#     """
-#     Checks formatResults with incomplete data for product
-#     """
-#     titles = [BeautifulSoup('<div class="title">Incomplete Data</div>', "html.parser")]
-#     prices = []
-#     links = [BeautifulSoup('<a href="http://example.com">link</a>', "html.parser")]
-#     images = []
-#     product = formatter.formatResult("example", titles, prices, links, images)
-#     assert product["price"] == "N/A" and product["title"] == "Incomplete Data"
 
 
 def test_sortList_empty_array():
